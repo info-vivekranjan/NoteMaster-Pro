1f39419cf92a7f6a8fdc3f8587fa3f4b2bf5402d
Vivek Ranjan <infovivek.ranjan@gmail.com>
2022-06-13 17:41:43 +0530

    updated readme and added .env

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Note-maker
+
+### Using this app anyone can create notes to maintain docs at one place.
+
+#### Here are some screen of the website
+
+* Home page
+![Homepage](./screenshots/home.png)
+
+* Notes page
+
+![Notes page](./screenshots/notes.png)
+
+* Create notes page
+
+![Create notes page](./screenshots/create.png)
+
+* Edit notes page
+![Edit notes page](./screenshots/edit.png)
+
+* Login page
+![Login page](./screenshots/login.png)
+
+* Register page
+![Register page](./screenshots/register.png)
+
+## Tech stack used
+  * React JS
+  * Node JS
+  * Express JS
+  * MongoDB
+  * Material UI
+  * JSON web token
+
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const env = require("dotenv");
 env.config();
+const path = require("path");
 const connect = require("./config/db");
 const userRoute = require("./routes/userRoute");
 const noteRoute = require("./routes/NoteRoute");
@@ -12,6 +13,22 @@ app.use(cors());
 
 app.use("/user", userRoute);
 app.use("/note", noteRoute);
+
+// --------------------------deployment------------------------------
+// const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
 
 app.listen(process.env.PORT, async () => {
   await connect();
