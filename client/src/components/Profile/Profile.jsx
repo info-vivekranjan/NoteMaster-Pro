253f1f1a832e50fb811573d3c184fae1a3068ee8
info-vivekranjan <infovivek.ranjan@gmail.com>
2024-06-09 20:57:53 +0530

    Profile page init

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { Box } from '@mui/material';
+import { Box, Paper, Typography } from "@mui/material";
 
 const Profile = () => {
+  const userProfile = JSON.parse(localStorage.getItem("userInfo"));
+
   return (
     <React.Fragment>
       <Navbar />
       <Box sx={{ pt: "100px" }}>
-        <div>Profile</div>
+        <Box sx={{ mb: "50px" }}>
+          <Paper elevation={3} sx={{ width: '40%', m: 'auto', p: '30px', textAlign: 'center', mt: '15vh' }}>
+            <Typography variant="h4"> Profile</Typography>
+            <img
+              alt="Profile"
+              src={userProfile?.pic}
+              style={{
+                width: "30%",
+                border: "2px solid black",
+                borderRadius: "50%",
+              }}
+            />
+            <Typography variant="h6">Name : {userProfile?.name}</Typography>
+            <Typography variant="h6">Email : {userProfile?.email}</Typography>
+          </Paper>
+        </Box>
       </Box>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
