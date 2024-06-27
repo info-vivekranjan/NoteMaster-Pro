019c5fa4cddc3a0b912c35ec042d8503feea0d95
info-vivekranjan <infovivek.ranjan@gmail.com>
2024-06-27 10:49:20 +0530

    Markdown editor added

---
+++ client/src/components/MarkdownEditor/DemoMarkdownCode.ts
@@ -0,0 +1,145 @@
+export const code = `
+# Project Title
+
+A brief description of what this project does and who it's for
+
+
+## Acknowledgements
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
+
+
+## Badges
+
+Add badges from somewhere like: [shields.io](https://shields.io/)
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+
+## Appendix
+
+Any additional information goes here
+
+
+## Authors
+
+- [@octokatherine](https://www.github.com/octokatherine)
+
+
+## Documentation
+
+[Documentation](https://linktodocumentation)
+
+## Color Reference
+
+| Color             | Hex                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| Example Color | ![#0a192f](https://via.placeholder.com/10/0a192f?text=+) #0a192f |
+| Example Color | ![#f8f8f8](https://via.placeholder.com/10/f8f8f8?text=+) #f8f8f8 |
+| Example Color | ![#00b48a](https://via.placeholder.com/10/00b48a?text=+) #00b48a |
+| Example Color | ![#00d1a0](https://via.placeholder.com/10/00b48a?text=+) #00d1a0 |
+
+
+## API Reference
+
+#### Get all items
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| "api_key" | "string" | **Required**. Your API key |
+
+#### Get item
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| "id"      | "string" | **Required**. Id of item to fetch |
+
+#### add(num1, num2)
+
+Takes two numbers and returns the sum.
+
+
+## Contributing
+
+Contributions are always welcome!
+
+See "contributing.md" for ways to get started.
+
+Please adhere to this project's "code of conduct".
+
+
+## Demo
+
+Insert gif or link to demo
+
+
+## Deployment
+
+To deploy this project run
+
+## FAQ
+
+#### Question 1
+
+Answer 1
+
+#### Question 2
+
+Answer 2
+
+
+## Features
+
+- Light/dark mode toggle
+- Live previews
+- Fullscreen mode
+- Cross platform
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at fake@fake.com
+
+
+## 🚀 About Me
+I'm a full stack developer...
+
+
+# Hi, I'm Katherine! 👋
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/)
+
+
+## Other Common Github Profile Sections
+👩‍💻 I'm currently working on...
+
+🧠 I'm currently learning...
+
+👯‍♀️ I'm looking to collaborate on...
+
+🤔 I'm looking for help with...
+
+💬 Ask me about...
+
+📫 How to reach me...
+
+😄 Pronouns...
+
+⚡️ Fun fact...
+
+
+## Installation
+
+Install my-project with npm
+    
+## 🛠 Skills
+Javascript, HTML, CSS...
+`;
--- client/src/components/MarkdownEditor/index.tsx
+++ client/src/components/MarkdownEditor/index.tsx
@@ -1,11 +1,15 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Navbar from "../Navbar/Navbar";
 // import styles from "./MarkdownEditor.module.scss";
+import MarkdownCodeEditor from "@uiw/react-markdown-editor";
+import { code } from "./DemoMarkdownCode";
 
 const MarkdownEditor = () => {
+  const [markdownVal, setMarkdownVal] = useState(code);
+  console.log("markdownVal:", markdownVal);
   let theme = createTheme({
     palette: {
       primary: {
@@ -13,6 +17,9 @@ const MarkdownEditor = () => {
       },
       secondary: {
         main: "#edf2ff",
+      },
+      black: {
+        main: "#000000",
       },
     },
   });
@@ -22,9 +29,34 @@ const MarkdownEditor = () => {
       <Navbar />
       <Box sx={{ pt: "100px" }}>
         <ThemeProvider theme={theme}>
-          <Container maxWidth="lg" sx={{ mb: "50px" }}>
-            <Box sx={{ mb: "50px" }}>
+          <Container maxWidth="xl" sx={{ mb: "25px" }}>
+            <Box sx={{ mb: "25px" }}>
               <Typography variant="h4">Markdown Editor</Typography>
+            </Box>
+            <Box>
+              <MarkdownCodeEditor
+                style={{ height: "70vh" }}
+                value={markdownVal}
+                onChange={(value) => {
+                  setMarkdownVal(value);
+                }}
+              />
+              <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    mt: "20px",
+                  }}
+                >
+                  <Button variant="contained" sx={{ mr: "20px" }}>
+                    Save Changes
+                  </Button>
+                  <Button variant="contained" color="black">
+                    <Box component="span" color="white">
+                      Cancel
+                    </Box>
+                  </Button>
+                </Box>
             </Box>
           </Container>
         </ThemeProvider>
